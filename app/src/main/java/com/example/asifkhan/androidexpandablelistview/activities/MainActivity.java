fc1e044ac34266e5ddbc3b29f1ac251bca2410a4
Asif Khan <asifkhan.github@gmail.com>
2018-09-18 16:02:34 +0600

    project initialized

---
+++ app/src/main/java/com/example/asifkhan/androidexpandablelistview/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.asifkhan.androidexpandablelistview.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asifkhan.androidexpandablelistview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
